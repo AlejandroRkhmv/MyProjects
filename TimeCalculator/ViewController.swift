002ee38df565402a90120a4e4cb3b73c744f3341
Alexander-Rakhimov <alexanderrkhmv@gmail.com>
2022-08-29 23:47:49 +0300

    add a general functionality

---
+++ TimeCalculator/ViewController.swift
@@ -0,0 +1,233 @@
+//
+//  ViewController.swift
+//  TimeCalculator
+//
+//  Created by Александр Рахимов on 28.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let model = OperandsCalculation()
+    
+    let possibleNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    var freeLabel = false
+    var hourButtonDidPressed = false
+    var minuteButtonDidPressed = false
+    var secondButtonDidPressed = false
+    
+    var hoursNumber = 0
+    var minuteNumber = 0
+    var secondNumber = 0
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        
+        let number = sender.titleLabel?.text
+        
+        if (resultLabel.text?.count)! < 19 {
+            if freeLabel {
+                resultLabel.text! += number!
+                
+            } else {
+                resultLabel.text = number
+                freeLabel = true
+            }
+        }
+    }
+    
+    func endIndexIsNumber() -> Bool {
+        let endIndexCharacter = resultLabel.text?.index(before: (resultLabel.text?.endIndex)!)
+        let endCharacter = resultLabel.text![endIndexCharacter!]
+        
+        return possibleNumbers.contains(String(endCharacter))
+    }
+    
+    @IBAction func hourButtonPressed(_ sender: UIButton) {
+        let hourTitle = sender.titleLabel?.text
+        
+        if (resultLabel.text?.count)! < 19 && endIndexIsNumber() {
+            if freeLabel && !hourButtonDidPressed && !minuteButtonDidPressed && !secondButtonDidPressed {
+                resultLabel.text! += hourTitle!
+                hourButtonDidPressed = true
+            }
+        }
+    }
+    
+    @IBAction func minuteButtonPressed(_ sender: UIButton) {
+        let minuteTitle = sender.titleLabel?.text
+        
+        if (resultLabel.text?.count)! < 19 && endIndexIsNumber() {
+            if freeLabel && !minuteButtonDidPressed && !secondButtonDidPressed {
+                resultLabel.text! += minuteTitle!
+                minuteButtonDidPressed = true
+            }
+        }
+    }
+    
+    
+    @IBAction func secondButtonPressed(_ sender: UIButton) {
+        let secondTitle = sender.titleLabel?.text
+        
+        if (resultLabel.text?.count)! < 19 && endIndexIsNumber() {
+            if freeLabel && !secondButtonDidPressed {
+                resultLabel.text! += secondTitle!
+                secondButtonDidPressed = true
+            }
+        }
+    }
+    
+    func calculatedHoursMinutesAndSeconds() {
+        if hourButtonDidPressed {
+            let hoursValue = resultLabel.text!.components(separatedBy: "h")
+            hoursNumber = Int((hoursValue.first)!) ?? 0
+            if hoursValue.count == 2 {
+                if minuteButtonDidPressed {
+                    let minutesValue = hoursValue[1].components(separatedBy: "m")
+                    minuteNumber = Int((minutesValue.first)!) ?? 0
+                    if minutesValue.count == 2 {
+                        if secondButtonDidPressed {
+                            let secondValue = minutesValue[1].components(separatedBy: "s")
+                            secondNumber = Int((secondValue.first)!) ?? 0
+                        }
+                    }
+                } else {
+                    minuteNumber = 0
+                        let secondValue =  hoursValue[1].components(separatedBy: "s")
+                        secondNumber = Int(secondValue.first!) ?? 0
+                }
+            }
+        } else {
+            hoursNumber = 0
+            if minuteButtonDidPressed {
+                let minutValue = resultLabel.text!.components(separatedBy: "m")
+                minuteNumber = Int(minutValue.first!) ?? 0
+                if minutValue.count == 2 {
+                    if secondButtonDidPressed {
+                        let secondValue = minutValue[1].components(separatedBy: "s")
+                        secondNumber = Int(secondValue.first!) ?? 0
+                    } else {
+                        secondNumber = 0
+                    }
+                }
+            } else {
+                minuteNumber = 0
+                if secondButtonDidPressed {
+                    let secondValue = resultLabel.text!.components(separatedBy: "s")
+                    secondNumber = Int(secondValue.first!) ?? 0
+                } else {
+                    secondNumber = 0
+                }
+            }
+        }
+    }
+    
+    @IBAction func twoOperandsButton(_ sender: UIButton) {
+        
+        model.binaryOperator = (sender.titleLabel?.text)!
+        
+        calculatedHoursMinutesAndSeconds()
+        
+        print(hoursNumber)
+        print(minuteNumber)
+        print(secondNumber)
+        
+        model.setFirstOperand(hours: hoursNumber, minutes: minuteNumber, seconds: secondNumber)
+        
+        //let firstOperandTextForLabel = resultLabel.text!
+        
+        hoursNumber = 0
+        minuteNumber = 0
+        secondNumber = 0
+        resultLabel.text = "00h00m00s"
+        freeLabel = false
+        
+        hourButtonDidPressed = false
+        minuteButtonDidPressed = false
+        secondButtonDidPressed = false
+        
+        
+        
+        print(model.firstOperand)
+        
+    }
+    
+    @IBAction func equalButton(_ sender: UIButton) {
+        
+        calculatedHoursMinutesAndSeconds()
+        
+        print(hoursNumber)
+        print(minuteNumber)
+        print(secondNumber)
+        
+        model.setSecondOperand(hours: hoursNumber, minutes: minuteNumber, seconds: secondNumber)
+        
+        print(model.secondOperand)
+        
+        let result = model.calculateAnswer()
+        
+        resultLabel.text = "\(result["h"]!)h\(result["m"]!)m\(result["s"]!)s"
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        hoursNumber = 0
+        minuteNumber = 0
+        secondNumber = 0
+        resultLabel.text = "00h00m00s"
+        freeLabel = false
+        
+        hourButtonDidPressed = false
+        minuteButtonDidPressed = false
+        secondButtonDidPressed = false
+    }
+    
+    @IBAction func deleteLastSymbol(_ sender: UIButton) {
+        
+        if resultLabel.text!.count > 1 && freeLabel {
+            
+            let endStringIndex = resultLabel.text!.index(before: (resultLabel.text!.endIndex))
+            let endCharacter = resultLabel.text!.remove(at: endStringIndex)
+            
+            switch endCharacter {
+            case "h":
+                hourButtonDidPressed = false
+            case "m":
+                minuteButtonDidPressed = false
+            case "s":
+                secondButtonDidPressed = false
+            default: break
+            }
+        } else if resultLabel.text!.count == 1 {
+            resultLabel.text = "00h00m00s"
+            
+            hoursNumber = 0
+            minuteNumber = 0
+            secondNumber = 0
+            resultLabel.text = "00h00m00s"
+            freeLabel = false
+            
+            hourButtonDidPressed = false
+            minuteButtonDidPressed = false
+            secondButtonDidPressed = false
+        }
+    }
+    
+}
+
---
+++ TimeCalculator/model/ModelTimeCalculator.swift
@@ -0,0 +1,58 @@
+//
+//  modelTimeCalculator.swift
+//  TimeCalculator
+//
+//  Created by Александр Рахимов on 28.08.2022.
+//
+
+import Foundation
+import UIKit
+
+class OperandsCalculation {
+    
+    var firstOperand = 0
+    var secondOperand = 0
+    var binaryOperator = ""
+    
+    private func calculateOperands(hours: Int, minutes: Int, seconds: Int) -> Int {
+        
+        let temporaryOperand = (hours * 3600) + (minutes * 60) + seconds
+        
+        return temporaryOperand
+    }
+    
+    public func setFirstOperand(hours: Int, minutes: Int, seconds: Int) {
+        
+        firstOperand = calculateOperands(hours: hours, minutes: minutes, seconds: seconds)
+        
+    }
+    
+    public func setSecondOperand(hours: Int, minutes: Int, seconds: Int) {
+        
+        secondOperand = calculateOperands(hours: hours, minutes: minutes, seconds: seconds)
+        
+    }
+    
+    
+    func calculateAnswer() -> [String: Int] {
+        
+        var resultToresultLabel = ["h": 0, "m": 0, "s": 0]
+        var currentResultInSeconds = 0
+        
+        switch binaryOperator {
+        case "+":
+            currentResultInSeconds = firstOperand + secondOperand
+        case "-":
+            currentResultInSeconds = firstOperand - secondOperand
+        default: break
+        }
+        
+        resultToresultLabel["h"] = currentResultInSeconds / 3600
+        resultToresultLabel["m"] = (currentResultInSeconds % 3600) / 60
+        resultToresultLabel["s"] = (currentResultInSeconds % 3600) % 60
+        
+        
+        
+        return resultToresultLabel
+    }
+}
